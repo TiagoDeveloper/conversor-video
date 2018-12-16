a7a27028003020b676692be17be949532dcce4af
tiagodeveloper <tiagopereirafonseca@gmail.com>
2018-12-16 20:03:29 -0200

    master: Tirando a lógica de negocio do zencoder do controller e passando para um service. Tirei tb a questão das chaves do zencoder e da amazon s3 do controller. Para a amazon S3, falta fazer o service, pois fiz apenas o bean para acesso.

--- src/main/java/com/tiagodeveloper/controller/MainController.java
+++ src/main/java/com/tiagodeveloper/controller/MainController.java
@@ -4,59 +4,35 @@ package com.tiagodeveloper.controller;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Arrays;
-
-import javax.annotation.PostConstruct;
+import java.net.URL;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
+import org.json.simple.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.tiagodeveloper.service.ZencoderService;
 
 @Controller
 @RequestMapping(value={"","/","/main"})
-@PropertySource(value={"credentials.properties"},ignoreResourceNotFound=true)
 public class MainController {
 	
-	private AWSCredentials credentials;
+	@Autowired
+	private AmazonS3 s3client;
 	
-	@Value("${basic.aws.credentials.accesskey}")
-	private String accessKey;
-
-	@Value("${basic.aws.credentials.secretkey}")
-	private String secretKey;
-
-	@Value("${Zencoder.api.key}")
-	private String zencoderApiKey;
-	
-	@PostConstruct
-	public void init() {
-		this.credentials = new BasicAWSCredentials(
-								accessKey, 
-								secretKey
-							);
-	}
-	
+	@Autowired
+	private ZencoderService zencoderService;
 	
 	
 	@GetMapping(value={"","/","/home"})
@@ -67,57 +43,40 @@ public class MainController {
 		return "home";
 	}
 	@PostMapping("/salvar")
-    public String handleFileUpload(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) throws IOException {
-
-		AmazonS3 s3client = AmazonS3ClientBuilder
-				.standard()
-				.withCredentials(new AWSStaticCredentialsProvider(credentials))
-				.withRegion(Regions.SA_EAST_1)
-				.build();
+    public @ResponseBody JSONObject handleFileUpload(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) throws IOException {
+		String bucketName = "aws-conversor-videos";
 		
 		File arq = new File(file.getOriginalFilename());
 		
-//		file.transferTo(arq);
 		FileUtils.copyInputStreamToFile(file.getInputStream(), arq);
 		
-		s3client.putObject("aws-conversor-videos", file.getOriginalFilename(), arq);
+		this.s3client.putObject(bucketName , file.getOriginalFilename(), arq);
+
+		URL url = this.s3client.getUrl(bucketName , file.getOriginalFilename());
 		
 		arq.delete();
 		
-		return "redirect:/";
+		return this.zencoderService.encodingJob(url.toString());
     }
 	
 	
 	
 	@GetMapping(value="/file", produces=MediaType.APPLICATION_OCTET_STREAM_VALUE)
 	public @ResponseBody byte[] teste() throws IOException{
-		AmazonS3 s3client = AmazonS3ClientBuilder
-				  .standard()
-				  .withCredentials(new AWSStaticCredentialsProvider(credentials))
-				  .withRegion(Regions.SA_EAST_1)
-				  .build();
 		InputStream in = s3client.getObject("aws-conversor-videos", "WhatsAppVideo.mp4").getObjectContent();
 		return IOUtils.toByteArray(in);
 		
 	}
 	
 	@GetMapping("/zencoder")
-	public String testeZen(){
-		RestTemplate restTemplate = new RestTemplate();
-		HttpHeaders headers = new HttpHeaders();
-
-		  headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-	      headers.set("Zencoder-Api-Key", zencoderApiKey);
-	      headers.setContentType(MediaType.APPLICATION_JSON);
-	      
-	      
-	      
-	    String input = "{\"input\": \"s3://s3-sa-east-1.amazonaws.com/aws-conversor-videos/video-teste.mp4\",\"outputs\": [{\"label\": \"mp4 high\",\"h264_profile\": \"high\"},{\"label\": \"webm\",\"format\": \"webm\"},{\"label\": \"ogg\",\"format\": \"ogg\"},{\"label\": \"mp4 low\",\"size\": \"640x480\"}]}";  
-
-	    HttpEntity<String> entity = new HttpEntity<String>(input,headers);
-		String teste = restTemplate.exchange("https://app.zencoder.com/api/v2/jobs", HttpMethod.POST, entity, String.class).getBody();
-		System.out.println(teste);
-		return "redirect:/";
+	public @ResponseBody JSONObject encodingJob(){
+		return this.zencoderService.encodingJob("s3://s3-sa-east-1.amazonaws.com/aws-conversor-videos/video-teste.mp4");
 	}
+	
+	@GetMapping("/verifyProgressOutput/{id}")
+	public @ResponseBody JSONObject verifyProgressOutput(@PathVariable("id") Long id){
+		return this.zencoderService.progressJobOutput(id);
+	}
+	
 
 }
